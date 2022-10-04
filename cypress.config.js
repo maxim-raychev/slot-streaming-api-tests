cedc9c4c97d2279f8ab6beafc72cfaece6280b38
maxraychev <maxraychev@ukr.net>
2022-10-04 07:22:12 +0300

    remove config verification

--- cypress.config.js
+++ cypress.config.js
@@ -7,16 +7,6 @@ const path = require('path')
 function getConfigurationByFile(file) {
   
   let pathToConfigFile = path.resolve(__dirname, 'cypress/config', `${file}.json`)
-  
-  // check if path is correct
-  const myArray = pathToConfigFile.split("/")
-  const lastDir = myArray.slice(-1)
-  if (lastDir !== 'config'){
-    pathToConfigFile = pathToConfigFile + 'cypress/config'
-    console.log(pathToConfigFile)
-  }
-
-  console.log(pathToConfigFile)
 
   return fs.readJson(pathToConfigFile)
 }
